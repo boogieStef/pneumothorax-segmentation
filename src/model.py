63d99a6f8b70df145e7b229d1541efa96ca38509
Stefan Bogdan <stefan7bogdan@gmail.com>
2026-01-18 16:32:29 +0100

    Fix model

--- src/model.py
+++ src/model.py
@@ -3,46 +3,24 @@ import torch.nn as nn
 import segmentation_models_pytorch as smp
 
 class ModelFactory:
-    """
-    Factory class to instantiate segmentation models using 
-    segmentation-models-pytorch (smp) library.
-    """
     @staticmethod
     def create(config):
-        """
-        Creates and returns a model instance based on the config.
-        
-        Args:
-            config (dict): Configuration dictionary.
-            
-        Returns:
-            nn.Module: The requested PyTorch model.
-        """
-        arch_name = config['train']['architecture'] # np. "Unet", "UnetPlusPlus", "DeepLabV3Plus"
-        encoder_name = config['train'].get('encoder_name', 'resnet34') # np. "resnet34", "efficientnet-b0"
+        arch_name = config['train']['architecture']
+        encoder_name = config['train'].get('encoder_name', 'resnet34')
         pretrained = config['train']['pretrained']
         
-        # Binary segmentation = 1 class
-        num_classes = 1 
-        
-        # Wybór wag (ImageNet)
         encoder_weights = "imagenet" if pretrained else None
         
-        print(f"[INFO] Initializing SMP Model: Arch={arch_name}, Encoder={encoder_name}, Weights={encoder_weights}")
-        
-        # Dynamiczne tworzenie modelu z biblioteki SMP
-        # Obsługuje: Unet, UnetPlusPlus, MAnet, Linknet, FPN, PSPNet, DeepLabV3, DeepLabV3Plus, PAN
+        # --- ZMIANA KLUCZOWA ---
+        # Wracamy do in_channels=1, tak jak w "dobrym kodzie".
+        # RTG to grayscale. SMP automatycznie dostosuje wagi pierwszej warstwy.
         model_fn = getattr(smp, arch_name, None)
-        
-        if model_fn is None:
-            raise ValueError(f"Architecture '{arch_name}' not found in segmentation-models-pytorch library.")
-            
         model = model_fn(
             encoder_name=encoder_name,
             encoder_weights=encoder_weights,
-            in_channels=1,
-            classes=num_classes,
-            activation=None # Zwracamy surowe logity (Loss function ma w sobie sigmoid)
+            in_channels=1,  # <--- TU BYŁO 3, ZMIENIAMY NA 1
+            classes=1,
+            activation=None
         )
         
         return model
